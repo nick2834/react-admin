e0509780e0d20c972a1e56250d695a16df38adbc
nickxu <283448189@qq.com>
2021-03-01 17:20:21 +0800

    侧边栏

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,34 +1,44 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Menu } from 'antd';
-import {
-    HomeOutlined,
-    UserOutlined,
-    MailOutlined,
-} from '@ant-design/icons';
 import './index.less';
 import logo from '@/assets/images/logo.png';
+import MenuList from '@/config/menuConfig'
 const { SubMenu } = Menu;
 export default class Sidebar extends Component {
+    getMenuNodes = (MenuList) => {
+        const path = window.location.pathname;
+        return MenuList.reduce((pre, item) => {
+            if (!item.children) {
+                pre.push(
+                    <Menu.Item key={item.key} icon={item.icon}><Link to={item.key}>{item.title}</Link></Menu.Item>
+                )
+            } else {
+                pre.push((
+                    <SubMenu key={item.key} icon={item.icon} title={item.title}>
+                        {this.getMenuNodes(item.children)}
+                    </SubMenu>
+                ))
+            }
+            return pre
+        }, [])
+    }
     render() {
+        const path = window.location.pathname;
         return (
             <div className="side_bar">
                 <div className="logo" >
                     <img src={logo} alt="" />
                 </div>
                 <Menu
-                    defaultSelectedKeys={['/home']}
+                    defaultSelectedKeys={[path]}
                     defaultOpenKeys={['sub1']}
                     mode="inline"
                     theme="dark"
                 >
-                    <Menu.Item key="/home" icon={<HomeOutlined />}><Link to="/home">首页</Link></Menu.Item>
-                    <SubMenu key="sub1" icon={<MailOutlined />} title="文章">
-                        <Menu.Item key="/articles"><Link to="/articles">文章管理</Link></Menu.Item>
-                        <Menu.Item key="/category"><Link to="/category">文章分类</Link></Menu.Item>
-                    </SubMenu>
-                    <Menu.Item key="/user" icon={<UserOutlined />}><Link to="/user">用户管理</Link></Menu.Item>
-                    <Menu.Item key="/role" icon={<UserOutlined />}><Link to="/role">角色管理</Link></Menu.Item>
+                    {
+                        this.getMenuNodes(MenuList)
+                    }
                 </Menu>
             </div>
         )
--- src/config/menuConfig.js
+++ src/config/menuConfig.js
@@ -1,37 +1,37 @@
+import { HomeOutlined, UserOutlined, TeamOutlined } from "@ant-design/icons";
 const menuList = [{
-        title: '首页', // 菜单标题名称
-        key: '/home', // 对应的path
-        icon: 'home', // 图标名称
+        title: "首页", // 菜单标题名称
+        key: "/home", // 对应的path
+        icon: < HomeOutlined / > , // 图标名称
         isPublic: true, // 公开的
     },
     {
-        title: '商品',
-        key: '/products',
-        icon: 'appstore',
-        children: [ // 子菜单列表
-            {
-                title: '品类管理',
-                key: '/category',
-                icon: 'bars'
+        title: "文章",
+        key: "/article",
+        icon: < HomeOutlined / > ,
+        children: [{
+                title: "品类管理",
+                key: "/category",
+                icon: < HomeOutlined / > ,
             },
             {
-                title: '商品管理',
-                key: '/product',
-                icon: 'tool'
+                title: "文章管理",
+                key: "/articles",
+                icon: < HomeOutlined / > ,
             },
-        ]
+        ],
     },
 
     {
-        title: '用户管理',
-        key: '/user',
-        icon: 'user'
+        title: "用户管理",
+        key: "/user",
+        icon: < UserOutlined / > ,
     },
     {
-        title: '角色管理',
-        key: '/role',
-        icon: 'safety',
-    }
-]
+        title: "角色管理",
+        key: "/role",
+        icon: < TeamOutlined / > ,
+    },
+];
 
-export default menuList
+export default menuList;
